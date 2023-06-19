9d03dff7d38c6bf961fb993686233b9f7eb4ea8f
Тетяна Деревянко <t.derevyanko1209@gmail.com>
2023-06-19 22:00:25 +0300

    Добавьте файлы проекта.

---
+++ 2004_годинник/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace _2004_годинник
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("SimSun", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTime.Location = new System.Drawing.Point(12, 9);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(91, 97);
+            this.labelTime.TabIndex = 0;
+            this.labelTime.Text = " ";
+            this.labelTime.Click += new System.EventHandler(this.labelTime_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 128);
+            this.Controls.Add(this.labelTime);
+            this.Name = "Form1";
+            this.Text = "Годинник";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTime;
+    }
+}
+
---
+++ 2004_годинник/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2004_годинник
+{
+    public partial class Form1 : Form
+    {
+
+        private static Timer vTimer = new Timer();
+
+        private void ShowTime(object vObj, EventArgs e)
+        {
+
+            labelTime.Text = DateTime.Now.ToLongTimeString();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+            labelTime.Text = DateTime.Now.ToLongTimeString();
+
+            vTimer.Tick += new EventHandler(ShowTime);
+
+            vTimer.Interval = 1000;
+
+            vTimer.Start();
+            if (DateTime.Now.Minute == 0 && DateTime.Now.Second == 0)
+            {
+                MessageBox.Show("Рівно "+ DateTime.Now.Hour+" година!", "Годинник");
+            }
+        }
+
+        private void labelTime_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
